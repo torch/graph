5ecdbac9f4598d1e6f4dd171093a29954cd846eb
koray kavukcuoglu <koray@kavukcuoglu.org>
2012-11-26 11:48:56 +0000

    put files into git

---
+++ test.lua
@@ -0,0 +1,25 @@
+require 'graph'
+dofile 'utils.lua'
+
+g=graph.Graph()
+root=graph.Node(10)
+n1=graph.Node(1)
+n2=graph.Node(2)
+g:add(graph.Edge(root,n1))
+g:add(graph.Edge(root,n2))
+nend = graph.Node(20)
+g:add(graph.Edge(n1,nend))
+g:add(graph.Edge(n2,nend))
+g:add(graph.Edge(nend,root))
+
+local i = 0
+print('======= BFS ==========')
+root:bfs(function(node) i=i+1;print('i='..i);print(node)end)
+print('======= DFS ==========')
+i = 0
+root:dfs(function(node) i=i+1;print('i='..i);print(node)end)
+
+print('======= topsort ==========')
+s,rg,rn = g:topsort()
+
+graph.dot(g)
---
+++ utils.lua
@@ -0,0 +1,17 @@
+
+require 'qtsvg'
+
+function graph.dot(g)
+	local gv = g:todot()
+	local fngv = os.tmpname()
+	local fgv = io.open(fngv,'w')
+	fgv:write(gv)
+	fgv:close()
+	local fnsvg = os.tmpname()
+	os.execute('dot -Tsvg -o ' .. fnsvg .. ' ' .. fngv)
+	local qs = qt.QSvgWidget(fnsvg)
+	qs:show()
+	os.remove(fngv)
+	os.remove(fnsvg)
+	-- print(fngv,fnpng)
+end
