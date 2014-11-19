b07a42e7cfa9953a1981d1d0bc455a59635fa58b
Jonathan J Hunt <jjhunt@google.com>
2014-11-19 20:49:45 +0000

    Working with annotations.

--- test_graphviz.lua
+++ test_graphviz.lua
@@ -21,4 +21,16 @@ function tests.layout()
                     "y coordinates should be ordered")
 end
 
+
+function tests.testDotEscape()
+  tester:assert(graph._dotEscape('red') == 'red', 'Don\'t escape single words')
+  tester:assert(graph._dotEscape('My label') == '"My label"',
+                'Use quotes for spaces')
+  tester:assert(graph._dotEscape('Non[an') == '"Non[an"',
+                'Use quotes for non-alpha characters')
+  tester:assert(graph._dotEscape('My\nnewline') == '"My\\nnewline"',
+                'Escape newlines')
+end
+
+
 return tester:add(tests):run()
