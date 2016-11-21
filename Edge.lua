d1b5a9e63a4a6612c75787b473434d18670f57ae
Karan Dwivedi <karan.dwivedi42@gmail.com>
2016-11-21 21:38:46 +0100

    Update Edge.lua
    Add edge weight

--- Edge.lua
+++ Edge.lua
@@ -4,7 +4,8 @@ No methods, just two fields, from and to.
 ]]--
 local Edge = torch.class('graph.Edge')
 
-function Edge:__init(from,to)
+function Edge:__init(from,to,weight)
    self.from = from
    self.to = to
+   self.weight = weight
 end
